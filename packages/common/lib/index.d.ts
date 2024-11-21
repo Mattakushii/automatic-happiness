bc24c54a93bdecf2ff89d5751d9d23958f63cc7a
Makishimu <bitar1337@gmail.com>
2024-11-21 15:34:01 +0400

    add vite config

---
+++ packages/common/lib/index.d.ts
@@ -0,0 +1,1 @@
+export declare const increment: (a: number, b: number) => number;
---
+++ packages/common/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import path from 'path';
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es"],
+      name: "common",
+    },
+    emptyOutDir: true,
+    outDir: "./build",
+    rollupOptions: {
+      // Externalize deps that shouldn't be bundled
+      external: ["react", "react-dom"],
+      output: {
+        // Global vars to use in UMD build for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  
+  plugins: [dts({insertTypesEntry: true}), react()],
+});
+
