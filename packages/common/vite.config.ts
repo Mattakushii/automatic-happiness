b2430dbceb9f9aff5cb5ccc3afa830f2dd1c6e5c
Makishimu <bitar1337@gmail.com>
2024-11-25 12:16:31 +0400

    feat: update client config

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
@@ -9,7 +9,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       fileName: "index",
       formats: ["es"],
-      name: "common",
+      name: "common"
     },
     emptyOutDir: true,
     outDir: "./build",
@@ -20,12 +20,11 @@ export default defineConfig({
         // Global vars to use in UMD build for externalized deps
         globals: {
           react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
   },
-  
-  plugins: [dts({insertTypesEntry: true}), react()],
-});
 
+  plugins: [dts({ insertTypesEntry: true }), react()]
+});
