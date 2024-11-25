fd00a05b13370bfd44886471a8a5e585db2b3caa
Makishimu <bitar1337@gmail.com>
2024-11-25 12:23:03 +0400

    feat: update vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src")
+      }
+    },
+    define: {
+      BUILD_VARIANT_SHOULD_BE_REPLACED: JSON.stringify(mode)
+    },
+    plugins: [react(), checker({ typescript: true })]
+  };
+});
